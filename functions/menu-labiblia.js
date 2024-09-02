6e4b930f35281ec83a37249eb6fe6254d2e858fc
Dev Diego <151096440+Dev-Diego@users.noreply.github.com>
2024-09-01 21:10:54 -0500

    Actualizar menu-labiblia.js

--- functions/menu-labiblia.js
+++ functions/menu-labiblia.js
@@ -8,23 +8,24 @@ async function labiblia() {
             logCommand(ctx);
 
             try {
-                const imageUrl = 'https://telegra.ph/file/18a85eceefa00853ae8a1.jpg?raw=true'; 
+                const imageUrl = 'https://telegra.ph/file/65d33e1257607d4b05a1c.jpg?raw=true'; 
                 const caption = `
-╭─╮︹︹⊹︹︹⊹︹︹⊹︹︹╭─╮
-    ⚘݄𖠵⃕⁖𖥔͢Menu ꪶ͢𝑩𝒐𝒕⋆᭄͙̈
-╚▭࣪▬ִ▭࣪▬ִ▭࣪▬ִ▭࣪▬ִ▭࣪▬ִ▭࣪▬▭╝
+© mᥱᥒᥙ ᥆𝖿іᥴіᥲᥣ ძᥱ ᥡᥲᥱm᥆rіᑲ᥆𝗍 ☁️
 
-╭ׅׄ̇─ׅ̻ׄ╮۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹
-├ׁ̟̇  「 Cᴏᴍᴀɴᴅᴏs +𝟷𝟾 」
-├━─━─━─≪≪✠≫≫─━─━─━╯
-├ׁ̟̇❍✎ /pack
-├ׁ̟̇❍✎ /pack2
-├ׁ̟̇❍✎ /pack3
-├ׁ̟̇❍✎ /videosxxxc
-├ׁ̟̇❍✎ /videosxxxc2
-├ׁ̟̇❍✎ /hentai
-├ׁ̟̇❍✎ /rule34 + texto
-໋ꥇ╰ׁ̻─ׅׄ ⊹ٜ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪ׄ߭꒷ٜ߭ׄ꒦۪ׄ߭꒷ٜׄ߭꒦
+┏━━━━━━━━━━━━━━━━━━━⫸
+┃╭──────────────────╸
+┃│ ✧ 𝗖𝗢𝗠𝗔𝗡𝗗𝗢𝗦 +18 ✧
+┃│
+┃│「🔥」 /pack
+┃│「🔥」 /pack2
+┃│「🔥」 /pack3
+┃│「🔥」 /videosxxxc
+┃│「🔥」 /videosxxxc2
+┃│「🔥」 /hentai
+┃│「🔥」 /rule34 + texto
+໋ꥇ┃╰──────────────────╸
+┗━━━━━━━━━━━━━━━━━━━⫸
+
 /help    /herramientas   /juegos`;
               ctx.replyWithPhoto({ url: imageUrl }, { caption: caption });                    
             } catch (error) {
