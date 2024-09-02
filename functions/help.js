c4000f3f75cdc1cc0767989f324c920e51c34819
Dev Diego <151096440+Dev-Diego@users.noreply.github.com>
2024-09-01 20:48:33 -0500

    Actualizar help.js

--- functions/help.js
+++ functions/help.js
@@ -22,10 +22,7 @@ const date = d.toLocaleDateString(locale, {day: 'numeric', month: 'long', year: 
 const userCount = Object.keys(userData).length;
 async function loading(ctx, response) {
     const hawemod = [
-        "╭ׅׄ̇─ׅ̻ׄ╮۪̇߭⊹߭̇︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇߭︹ׅ۪ׄ̇߭̇⊹\n├ ⚘݄𖠵⃕⁖𖥔.Ƈᴀʀɢᴀɴᴅᴏ,  ꪶꪾ❍̵̤̂̂ꫂ\n├Ąɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ❞\n├■■▣□□□□□□□   25%\n╰ׁ̻۫─۪۬─۟─۪─۟─۪۬─۟─۪─۟─۪۬─۟─۪─۟─۪۬─۟─۪─۟┄۪۬┄۟┄۪┈۟┈۪",
-        "╭ׅׄ̇─ׅ̻ׄ╮۪̇߭⊹߭̇︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇߭︹ׅ۪ׄ̇߭̇⊹\n├ ⚘݄𖠵⃕⁖𖥔.Ƈᴀʀɢᴀɴᴅᴏ,  ꪶꪾ❍̵̤̂̂ꫂ\n├Ąɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ❞\n├■■■■■▣□□□□   55%\n╰ׁ̻۫─۪۬─۟─۪─۟─۪۬─۟─۪─۟─۪۬─۟─۪─۟─۪۬─۟─۪─۟┄۪۬┄۟┄۪┈۟┈۪",
-        "╭ׅׄ̇─ׅ̻ׄ╮۪̇߭⊹߭̇︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇߭︹ׅ۪ׄ̇߭̇⊹\n├ ⚘݄𖠵⃕⁖𖥔.Ƈᴀʀɢᴀɴᴅᴏ,  ꪶꪾ❍̵̤̂̂ꫂ\n├Ąɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ❞\n├■■■■■■■■▣□   85%\n╰ׁ̻۫─۪۬─۟─۪─۟─۪۬─۟─۪─۟─۪۬─۟─۪─۟─۪۬─۟─۪─۟┄۪۬┄۟┄۪┈۟┈۪",
-        "╭ׅׄ̇─ׅ̻ׄ╮۪̇߭⊹߭̇︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇߭︹ׅ۪ׄ̇߭̇⊹\n├ ⚘݄𖠵⃕⁖𖥔.Ƈᴀʀɢᴀɴᴅᴏ,  ꪶꪾ❍̵̤̂̂ꫂ\n├Ąɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ❞\n├■■■■■■■■■■   100%\n╰ׁ̻۫─۪۬─۟─۪─۟─۪۬─۟─۪─۟─۪۬─۟─۪─۟─۪۬─۟─۪─۟┄۪۬┄۟┄۪┈۟┈۪"
+        "🌸 Enviando el menú... 👑"
     ];
     
    const message = await ctx.reply('Aguarde', { mentions: ctx.message });
@@ -47,32 +44,35 @@ async function helper(ctx) {
                 const diff = Date.now() - startTime;
         const uptime = clockString(diff);
                 const loadingMessage = await ctx.reply(`Hola ${ctx.message.from.username}, enviando menú...`);
-                const imageUrl = 'https://telegra.ph/file/aee88a66e3c5970a1c9ea.jpg?raw=true';
+                const imageUrl = 'https://telegra.ph/file/65d33e1257607d4b05a1c.jpg?raw=true';
                 const caption = `
-				╭─╮︹︹⊹︹︹⊹︹︹⊹︹︹╭─╮
-    ⚘݄𖠵⃕⁖𖥔͢Menu ꪶ͢𝑩𝒐𝒕⋆᭄͙̈
-╚▭࣪▬ִ▭࣪▬ִ▭࣪▬ִ▭࣪▬ִ▭࣪▬ִ▭࣪▬▭╝
+© mᥱᥒᥙ ᥆𝖿іᥴіᥲᥣ ძᥱ ᥡᥲᥱm᥆rіᑲ᥆𝗍 ☁️
 
-╭࣭࣭࣭࣭࣭࣭ٜ۫┄̸࣭࣭࣭࣭࣭ٜ۫☪︎︎︎̸⃘̸࣭ٜ࣪࣪࣪۬◌⃘۪֟፝֯۫۫︎⃪𐇽۫۬🌻⃘⃪۪֟፝֯۫۫۫۬◌⃘࣭ٜ࣪࣪࣪۬☪︎︎︎︎̸┄̸࣭࣭࣭࣭࣭ٜ۫┄࣭࣭࣭۫┄̸࣭࣭࣭࣭࣭ٜ۫┄࣭࣭࣭۫┄̸࣭۫┄̸࣭࣭࣭࣭࣭ٜ۫┄̸࣭۫┄̸࣭࣭࣭࣭࣭ٜ۫┄࣭࣭࣭۫╮
-     *I N F O*
-⬪࣪ꥈ𑁍⃪࣭۪ٜ݊݊݊݊݊໑ٜ࣪⚘۪۬Cʀᴇᴀᴅᴏʀ: DavidChian
-⬪࣪ꥈ𑁍⃪࣭۪ٜ݊݊݊݊݊໑ٜ࣪⚘۪۬Nᴜᴍᴇʀᴏ: https://t.me/DavidChian
-⬪࣪ꥈ𑁍⃪࣭۪ٜ݊݊݊݊݊໑ٜ࣪⚘۪۬Tɪᴇᴍᴘᴏ ᴀᴄᴛɪᴠᴏ: ${uptime}
-⬪࣪ꥈ𑁍⃪࣭۪ٜ݊݊݊݊݊໑ٜ࣪⚘۪۬Gʀᴜᴘᴏ ᴏғɪᴄɪᴀʟ: https://t.me/Kotori_OFC
-⬪࣪ꥈ𑁍⃪࣭۪ٜ݊݊݊݊݊໑ٜ࣪⚘۪۬Fᴇᴄʜᴀ: ${date}
-⬪࣪ꥈ𑁍⃪࣭۪ٜ݊݊݊݊݊໑ٜ࣪⚘۪۬Usᴜᴀʀɪᴏs ᴛᴏᴛᴀʟᴇs: ${userCount}
-╰┄̸࣭࣭࣭࣭࣭ٜ۫┄࣭࣭࣭۫┄̸࣭۫┄̸࣭࣭࣭࣭࣭ٜ۫┄࣭࣭࣭۫┄̸࣭۫┄̸࣭࣭࣭࣭࣭ٜ۫┄̸࣭࣭࣭࣭࣭ٜ۫┄࣭࣭࣭۫☪︎︎︎̸⃘̸࣭ٜ࣪࣪࣪۬◌⃘۪֟፝֯۫۫︎⃪𐇽۫۬🍧⃘⃪۪֟፝֯۫۫۫۬◌⃘࣭ٜ࣪࣪࣪۬☪︎︎︎︎̸┄̸࣭࣭࣭࣭࣭ٜ۫╯
+┏━━━━━━━━━━━━━━━━━━━⫸
+┃╭──────────────────╸
+┃│ ✧ 𝗜𝗡𝗙𝗢𝗥𝗠𝗔𝗖𝗜𝗢𝗡 ✧
+┃│
+┃│「🌱」 𝐂𝐫𝐞𝐚𝐝𝐨𝐫: DecDiego
+┃│「💫」 𝐍𝐮𝐦𝐞𝐫𝐨: https://t.me/Oficial_Diego
+┃│「🕒」 𝐀𝐜𝐭𝐢𝐯𝐢𝐝𝐚𝐝: ${uptime}
+┃│「🌴」 𝐆𝐫𝐮𝐩𝐨: https://t.me/YaemoriGrupo
+┃│「📅」 𝐅𝐞𝐜𝐡𝐚: ${date}
+┃│「👤」 𝐔𝐬𝐮𝐚𝐫𝐢𝐨𝐬: ${userCount}
+┃╰──────────────────╸
+┗━━━━━━━━━━━━━━━━━━━⫸
 
-╭ׅׄ̇─ׅ̻ׄ╮۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹
-├ׁ̟̇     「 Iɴғᴏ 」
-├━─━─━─≪≪✠≫≫─━─━─━╯
-├ׁ̟̇❍✎ /obtenidos
-├ׁ̟̇❍✎ /creador
-├ׁ̟̇❍✎ /grupos
-໋ꥇ╰ׁ̻─ׅׄ ⊹ٜ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪ׄ߭꒷ٜ߭ׄ꒦۪ׄ߭꒷ٜׄ߭꒦`;
+┏━━━━━━━━━━━━━━━━━━━⫸
+┃╭──────────────────╸
+┃│ ✧ 𝗖𝗢𝗠𝗔𝗡𝗗𝗢𝗦 ✧
+┃│
+┃│「👤」 /obtenidos
+┃│「👤」 /creador
+┃│「👤」 /grupos໋ꥇ
+┃╰──────────────────╸
+┗━━━━━━━━━━━━━━━━━━━⫸`;
                             
 
-                await loading(ctx, "╭ׅׄ̇─ׅ̻ׄ╮۪̇߭⊹߭̇︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇߭︹ׅ۪ׄ̇߭̇⊹\n├ ⚘݄𖠵⃕⁖𖥔.Ƈᴀʀɢᴀɴᴅᴏ,  ꪶꪾ❍̵̤̂̂ꫂ\n├Ąɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ❞\n├❦         𝐶𝑜𝑚𝑝𝑙𝑒𝑡𝑎𝑑𝑜 ✓\n╰ׁ̻۫─۪۬─۟─۪─۫─۪۬─۟─۪─۟─۪۬─۟─۪─۟─۪۬─۟─۪─۟┄۪۬┄۟┄۪┈۟┈۪");
+                await loading(ctx, "👑 Aquí está el menú oficial 🌸");
    
                 ctx.replyWithPhoto({ url: imageUrl }, {
                     caption: caption,
@@ -94,126 +94,132 @@ async function helper(ctx) {
         });
 		 DC.action('group', async (ctx) => {
             await ctx.telegram.sendChatAction(ctx.chat.id, 'typing');
-            await ctx.replyWithPhoto({ url: 'https://telegra.ph/file/58e670ea08aafdda5fd42.jpg' }, { caption: `
-╭─╮︹︹⊹︹︹⊹︹︹⊹︹︹╭─╮
-    ⚘݄𖠵⃕⁖𖥔͢Menu ꪶ͢𝑩𝒐𝒕⋆᭄͙̈
-╚▭࣪▬ִ▭࣪▬ִ▭࣪▬ִ▭࣪▬ִ▭࣪▬ִ▭࣪▬▭╝
+            await ctx.replyWithPhoto({ url: 'https://telegra.ph/file/65d33e1257607d4b05a1c.jpg' }, { caption: `
+© mᥱᥒᥙ ᥆𝖿іᥴіᥲᥣ ძᥱ ᥡᥲᥱm᥆rіᑲ᥆𝗍 ☁️
 
-╭ׅׄ̇─ׅ̻ׄ╮۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹
-├ׁ̟̇  「 Gʀᴜᴘᴏs 」
-├━─━─━─≪≪✠≫≫─━─━─━╯
-├ׁ̟̇❍✎ /antilink + on o off
-├ׁ̟̇❍✎ /modohorny + on o off
-├ׁ̟̇❍✎ /fijar + responder mensaje
-├ׁ̟̇❍✎ /kick + responer usuario
-໋ꥇ╰ׁ̻─ׅׄ ⊹ٜ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪ׄ߭꒷ٜ߭ׄ꒦۪ׄ߭꒷ٜׄ߭꒦
+┏━━━━━━━━━━━━━━━━━━━⫸
+┃╭──────────────────╸
+┃│ ✧ 𝗚𝗥𝗨𝗣𝗢𝗦 ✧
+┃│
+┃│「✨️」 /antilink + on o off
+┃│「✨️」 /modohorny + on o off
+┃│「✨️」 /fijar + responder mensaje
+┃│「✨️」 /kick + responer usuario
+໋ꥇ┃╰──────────────────╸
+┗━━━━━━━━━━━━━━━━━━━⫸
+
 /help    /juegos   /descargas` });
              });
          DC.action('herramientas', async (ctx) => {
             await ctx.telegram.sendChatAction(ctx.chat.id, 'typing');
-            await ctx.replyWithPhoto({ url: 'https://telegra.ph/file/58e670ea08aafdda5fd42.jpg' }, { caption: `
-╭─╮︹︹⊹︹︹⊹︹︹⊹︹︹╭─╮
-    ⚘݄𖠵⃕⁖𖥔͢Menu ꪶ͢𝑩𝒐𝒕⋆᭄͙̈
-╚▭࣪▬ִ▭࣪▬ִ▭࣪▬ִ▭࣪▬ִ▭࣪▬ִ▭࣪▬▭╝
+            await ctx.replyWithPhoto({ url: 'https://telegra.ph/file/65d33e1257607d4b05a1c.jpg' }, { caption: `
+© mᥱᥒᥙ ᥆𝖿іᥴіᥲᥣ ძᥱ ᥡᥲᥱm᥆rіᑲ᥆𝗍 ☁️
 
-╭ׅׄ̇─ׅ̻ׄ╮۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹
-├ׁ̟̇  「 Hᴇʀʀᴀᴍɪᴇɴᴛᴀs 」
-├━─━─━─≪≪✠≫≫─━─━─━╯
-├ׁ̟̇❍✎ /wiki + texto_
-├ׁ̟̇❍✎ /translate +identificador + texto
-├ׁ̟̇❍✎ /identificadores
-├ׁ̟̇❍✎ /spanishto + texto
-├ׁ̟̇❍✎ /ia + texto
-├ׁ̟̇❍✎ /dalle + texto
-├ׁ̟̇❍✎ /dalle2 + texto
-├ׁ̟̇❍✎ /tourl + responde a una imagen o video
-໋ꥇ╰ׁ̻─ׅׄ ⊹ٜ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪ׄ߭꒷ٜ߭ׄ꒦۪ׄ߭꒷ٜׄ߭꒦
+┏━━━━━━━━━━━━━━━━━━━⫸
+┃╭──────────────────╸
+┃│ ✧ 𝗛𝗘𝗥𝗥𝗔𝗠𝗜𝗘𝗡𝗧𝗔𝗦 ✧
+┃│
+┃│「👑」 /wiki + texto_
+┃│「👑」 /translate +identificador + texto
+┃│「👑」/identificadores
+┃│「👑」 /spanishto + texto
+┃│「👑」 /ia + texto
+┃│「👑」 /dalle + texto
+┃│「👑」 /dalle2 + texto
+┃│「👑」 /tourl + responde a una imagen o video
+┃╰──────────────────╸
+┗━━━━━━━━━━━━━━━━━━━⫸
+
 /help    /juegos   /descargas` });
              });
              DC.action('juegos', async (ctx) => {
             await ctx.telegram.sendChatAction(ctx.chat.id, 'typing');
-            await ctx.replyWithPhoto({ url: 'https://telegra.ph/file/58e670ea08aafdda5fd42.jpg' }, { caption: `
-╭─╮︹︹⊹︹︹⊹︹︹⊹︹︹╭─╮
-    ⚘݄𖠵⃕⁖𖥔͢Menu ꪶ͢𝑩𝒐𝒕⋆᭄͙̈
-╚▭࣪▬ִ▭࣪▬ִ▭࣪▬ִ▭࣪▬ִ▭࣪▬ִ▭࣪▬▭╝
+            await ctx.replyWithPhoto({ url: 'https://telegra.ph/file/65d33e1257607d4b05a1c.jpg' }, { caption: `
+© mᥱᥒᥙ ᥆𝖿іᥴіᥲᥣ ძᥱ ᥡᥲᥱm᥆rіᑲ᥆𝗍 ☁️
 
-╭ׅׄ̇─ׅ̻ׄ╮۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹
-├ׁ̟̇  「 Jᴜᴇɢᴏs 」
-├━─━─━─≪≪✠≫≫─━─━─━╯
-├ׁ̟̇❍✎ /cum + mencion
-├ׁ̟̇❍✎ /violar + mencion
-├ׁ̟̇❍✎ /nalguear + mencion
-├ׁ̟̇❍✎ /sexo + mencion
-├ׁ̟̇❍✎ /kiss + mencion
-├ׁ̟̇❍✎ /qc + texto
-├ׁ̟̇❍✎ /rw
-├ׁ̟̇❍✎ /love + responde a un usuario
-໋ꥇ╰ׁ̻─ׅׄ ⊹ٜ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪ׄ߭꒷ٜ߭ׄ꒦۪ׄ߭꒷ٜׄ߭꒦
+┏━━━━━━━━━━━━━━━━━━━⫸
+┃╭──────────────────╸
+┃│ ✧ 𝗝𝗨𝗘𝗚𝗢𝗦 ✧
+┃│
+┃│「🌸」 /cum + mencion
+┃│「🌸」 /violar + mencion
+┃│「🌸」 /nalguear + mencion
+┃│「🌸」 /sexo + mencion
+┃│「🌸」 /kiss + mencion
+┃│「🌸」 /qc + texto
+┃│「🌸」 /rw
+┃│「🌸」 /love + responde a un usuario
+໋ꥇ┃╰──────────────────╸
+┗━━━━━━━━━━━━━━━━━━━⫸
+
 /help    /random   /descargas` });
                  });
                   DC.action('descargas', async (ctx) => {
             await ctx.telegram.sendChatAction(ctx.chat.id, 'typing');
-            await ctx.replyWithPhoto({ url: 'https://telegra.ph/file/58e670ea08aafdda5fd42.jpg' }, { caption: `
-╭─╮︹︹⊹︹︹⊹︹︹⊹︹︹╭─╮
-    ⚘݄𖠵⃕⁖𖥔͢Menu ꪶ͢𝑩𝒐𝒕⋆᭄͙̈
-╚▭࣪▬ִ▭࣪▬ִ▭࣪▬ִ▭࣪▬ִ▭࣪▬ִ▭࣪▬▭╝
+            await ctx.replyWithPhoto({ url: 'https://telegra.ph/file/65d33e1257607d4b05a1c.jpg' }, { caption: `
+© mᥱᥒᥙ ᥆𝖿іᥴіᥲᥣ ძᥱ ᥡᥲᥱm᥆rіᑲ᥆𝗍 ☁️
 
-╭ׅׄ̇─ׅ̻ׄ╮۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹
-├ׁ̟̇  「 Dᴇsᴄᴀʀɢᴀs 」
-├━─━─━─≪≪✠≫≫─━─━─━╯
-├ׁ̟̇❍✎ /twitter *<enlace / link / url>*
-├ׁ̟̇❍✎ /mp3 *<enlace / link / url>*
-├ׁ̟̇❍✎ /mp4 *<enlace / link / url>*
-├ׁ̟̇❍✎ /tiktoksearch *<enlace / link / url>*
-├ׁ̟̇❍✎ /peliculainfo *<nombre>*
-├ׁ̟̇❍✎ /youtubechannel *<nombre>*
-├ׁ̟̇❍✎ /googledrive *<enlace / link / url>*
-├ׁ̟̇❍✎ /mediafire *<enlace / link / url>*
-├ׁ̟̇❍✎ /tiktokimg *<enlace / link / url>*
-├ׁ̟̇❍✎ /instagramstory *<enlace / link / url>*
-├ׁ̟̇❍✎ /instagram *<enlace / link / url>*
-├ׁ̟̇❍✎ /tiktok *<enlace / link / url>*
-├ׁ̟̇❍✎ /applemusic *<enlace / link / url>*
-├ׁ̟̇❍✎ /spotify *<enlace / link / url>*
-├ׁ̟̇❍✎ /facebook *<enlace / link / url>*
-├ׁ̟̇❍✎ /gitclone *<enlace / link / url>*
-໋ꥇ╰ׁ̻─ׅׄ ⊹ٜ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪ׄ߭꒷ٜ߭ׄ꒦۪ׄ߭꒷ٜׄ߭꒦
+┏━━━━━━━━━━━━━━━━━━━⫸
+┃╭──────────────────╸
+┃│ ✧ 𝗗𝗘𝗦𝗖𝗔𝗥𝗚𝗔𝗦 ✧
+┃│
+┃│「🌺」 /twitter *<enlace / link / url>*
+┃│「🌺」 /mp3 *<enlace / link / url>*
+┃│「🌺」 /mp4 *<enlace / link / url>*
+┃│「🌺」 /tiktoksearch *<enlace / link / url>*
+┃│「🌺」 /peliculainfo *<nombre>*
+┃│「🌺」 /youtubechannel *<nombre>*
+┃│「🌺」 /googledrive *<enlace / link / url>*
+┃│「🌺」 /mediafire *<enlace / link / url>*
+┃│「🌺」 /tiktokimg *<enlace / link / url>*
+┃│「🌺」 /instagramstory *<enlace / link / url>*
+┃│「🌺」 /instagram *<enlace / link / url>*
+┃│「🌺」 /tiktok *<enlace / link / url>*
+┃│「🌺」 /applemusic *<enlace / link / url>*
+┃│「🌺」 /spotify *<enlace / link / url>*
+┃│「🌺」 /facebook *<enlace / link / url>*
+┃│「🌺」 /gitclone *<enlace / link / url>*
+໋ꥇ┃╰──────────────────╸
+┗━━━━━━━━━━━━━━━━━━━⫸
+
 /help    /labiblia   /juegos` });
                       });
                        DC.action('random', async (ctx) => {
             await ctx.telegram.sendChatAction(ctx.chat.id, 'typing');
-            await ctx.replyWithPhoto({ url: 'https://telegra.ph/file/58e670ea08aafdda5fd42.jpg' }, { caption: `
-╭─╮︹︹⊹︹︹⊹︹︹⊹︹︹╭─╮
-    ⚘݄𖠵⃕⁖𖥔͢Menu ꪶ͢𝑩𝒐𝒕⋆᭄͙̈
-╚▭࣪▬ִ▭࣪▬ִ▭࣪▬ִ▭࣪▬ִ▭࣪▬ִ▭࣪▬▭╝
+            await ctx.replyWithPhoto({ url: 'https://telegra.ph/file/65d33e1257607d4b05a1c.jpg' }, { caption: `
+© mᥱᥒᥙ ᥆𝖿іᥴіᥲᥣ ძᥱ ᥡᥲᥱm᥆rіᑲ᥆𝗍 ☁️
 
-╭ׅׄ̇─ׅ̻ׄ╮۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹
-├ׁ̟̇  「 Rᴀɴᴅᴏᴍ 」
-├━─━─━─≪≪✠≫≫─━─━─━╯
-├ׁ̟̇❍✎ /randomanime
-├ׁ̟̇❍✎ /walldesktop
-├ׁ̟̇❍✎ /wallphone
-໋ꥇ╰ׁ̻─ׅׄ ⊹ٜ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪ׄ߭꒷ٜ߭ׄ꒦۪ׄ߭꒷ٜׄ߭꒦
+┏━━━━━━━━━━━━━━━━━━━⫸
+┃╭──────────────────╸
+┃│ ✧ 𝗥𝗔𝗡𝗗𝗢𝗠 ✧
+┃│
+┃│「⚡️」 /randomanime
+┃│「⚡️」 /walldesktop
+┃│「⚡️」 /wallphone
+໋ꥇ┃╰──────────────────╸
+┗━━━━━━━━━━━━━━━━━━━⫸
+
 /help    /herramientas   /descargas` });
                            });
                             DC.action('adult', async (ctx) => {
             await ctx.telegram.sendChatAction(ctx.chat.id, 'typing');
-            await ctx.replyWithPhoto({ url: 'https://telegra.ph/file/58e670ea08aafdda5fd42.jpg' }, { caption: `
-╭─╮︹︹⊹︹︹⊹︹︹⊹︹︹╭─╮
-    ⚘݄𖠵⃕⁖𖥔͢Menu ꪶ͢𝑩𝒐𝒕⋆᭄͙̈
-╚▭࣪▬ִ▭࣪▬ִ▭࣪▬ִ▭࣪▬ִ▭࣪▬ִ▭࣪▬▭╝
+            await ctx.replyWithPhoto({ url: 'https://telegra.ph/file/65d33e1257607d4b05a1c.jpg' }, { caption: `
+© mᥱᥒᥙ ᥆𝖿іᥴіᥲᥣ ძᥱ ᥡᥲᥱm᥆rіᑲ᥆𝗍 ☁️
 
-╭ׅׄ̇─ׅ̻ׄ╮۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹
-├ׁ̟̇  「 Cᴏᴍᴀɴᴅᴏs +𝟷𝟾 」
-├━─━─━─≪≪✠≫≫─━─━─━╯
-├ׁ̟̇❍✎ /pack
-├ׁ̟̇❍✎ /pack2
-├ׁ̟̇❍✎ /pack3
-├ׁ̟̇❍✎ /videosxxxc
-├ׁ̟̇❍✎ /videosxxxc2
-├ׁ̟̇❍✎ /hentai
-├ׁ̟̇❍✎ /rule34 + texto
-໋ꥇ╰ׁ̻─ׅׄ ⊹ٜ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪߭ׄ꒷ٜ߭ׄ꒦۪ׄ߭꒷ٜ߭ׄ꒦۪ׄ߭꒷ٜׄ߭꒦
+┏━━━━━━━━━━━━━━━━━━━⫸
+┃╭──────────────────╸
+┃│ ✧ 𝗖𝗢𝗠𝗔𝗡𝗗𝗢𝗦 +18 ✧
+┃│
+┃│「🔥」 /pack
+┃│「🔥」 /pack2
+┃│「🔥」 /pack3
+┃│「🔥」 /videosxxxc
+┃│「🔥」 /videosxxxc2
+┃│「🔥」 /hentai
+┃│「🔥」 /rule34 + texto
+໋ꥇ┃╰──────────────────╸
+┗━━━━━━━━━━━━━━━━━━━⫸
+
 /help    /herramientas   /juegos` })
                                 });
     } catch (error) {
