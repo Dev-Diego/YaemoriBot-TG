ea9b77156d37c4ae2ef2e7524c051b2f4650162e
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-10-02 09:42:08 -0500

    Actualizar global.js

--- global.js
+++ global.js
@@ -1,8 +1,7 @@
-
-const { Telegraf } = require('telegraf');
+/*const { Telegraf } = require('telegraf');
 require('dotenv').config();
 const TOKEN = (process.env.TOKEN);
 
 const DC = new Telegraf(TOKEN);
 
-module.exports = DC
+module.exports = DC*/
