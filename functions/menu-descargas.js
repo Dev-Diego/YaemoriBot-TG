46a83c9204e46d955355c53e96e93cf9c65569ab
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-10-12 16:18:04 -0500

    Actualizar menu-descargas.js

--- functions/menu-descargas.js
+++ functions/menu-descargas.js
@@ -9,24 +9,24 @@ async function descargas() {
 
             try {
                 const imageUrl = 'https://qu.ax/CmyNh.jpg?raw=true'; 
-                const caption = `•/• *Descargas* •/•
+                const caption = `•/• Descargas •/•
 
-✰ #twitter *<enlace / link / url>*
-✰ #mp3 *<enlace / link / url>*
-✰ #mp4 *<enlace / link / url>*
-✰ #tiktoksearch *<enlace / link / url>*
-✰ #peliculainfo *<nombre>*
-✰ #youtubechannel *<nombre>*
-✰ #googledrive *<enlace / link / url>*
-✰ #mediafire *<enlace / link / url>*
-✰ #tiktokimg *<enlace / link / url>*
-✰ #instagramstory *<enlace / link / url>*
-✰ #instagram *<enlace / link / url>*
-✰ #tiktok *<enlace / link / url>*
-✰ #applemusic *<enlace / link / url>
-✰ #spotify *<enlace / link / url>*
-✰ #facebook *<enlace / link / url>*
-✰ #gitclone *<enlace / link / url>*`;
+✰ #twitter <enlace / link / url>
+✰ #mp3 <enlace / link / url>
+✰ #mp4 <enlace / link / url>
+✰ #tiktoksearch <enlace / link / url>
+✰ #peliculainfo <nombre>
+✰ #youtubechannel <nombre>
+✰ #googledrive <enlace / link / url>
+✰ #mediafire <enlace / link / url>
+✰ #tiktokimg <enlace / link / url>
+✰ #instagramstory *<enlace / link / url>
+✰ #instagram <enlace / link / url>
+✰ #tiktok <enlace / link / url>
+✰ #applemusic <enlace / link / url>
+✰ #spotify <enlace / link / url>
+✰ #facebook <enlace / link / url>
+✰ #gitclone <enlace / link / url>`;
               ctx.replyWithPhoto({ url: imageUrl }, { caption: caption });                    
             } catch (error) {
                 dc.warn("Hay un error");
