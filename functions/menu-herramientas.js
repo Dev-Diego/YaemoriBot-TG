5b325eb3407d578e5f107c743e94b9e2de149d02
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-10-12 11:58:28 -0500

    Actualizar menu-herramientas.js

--- functions/menu-herramientas.js
+++ functions/menu-herramientas.js
@@ -8,33 +8,24 @@ async function herramientas() {
             logCommand(ctx);
 
             try {
-                const imageUrl = 'https://telegra.ph/file/65d33e1257607d4b05a1c.jpg?raw=true'; 
-                const caption = `
-© mᥱᥒᥙ ᥆𝖿іᥴіᥲᥣ ძᥱ ᥡᥲᥱm᥆rіᑲ᥆𝗍 ☁️
+                const imageUrl = 'https://qu.ax/CmyNh.jpg?raw=true'; 
+                const caption = `•/• *MENÚ HERRAMIENTAS* •/•
 
-┏━━━━━━━━━━━━━━━━━━━⫸
-┃╭──────────────────╸
-┃│ ✧ 𝗛𝗘𝗥𝗥𝗔𝗠𝗜𝗘𝗡𝗧𝗔𝗦 ✧
-┃│
-┃│「👑」 /wiki + texto_
-┃│「👑」 /translate +identificador + texto
-┃│「👑」/identificadores
-┃│「👑」 /spanishto + texto
-┃│「👑」 /ia + texto
-┃│「👑」 /dalle + texto
-┃│「👑」 /dalle2 + texto
-┃│「👑」 /tourl + responde a una imagen o video
-┃╰──────────────────╸
-┗━━━━━━━━━━━━━━━━━━━⫸
-
-/help    /juegos   /descargas`;
+✰ #wiki + texto
+✰ #translate +identificador + texto
+✰ #identificadores
+✰ #spanishto + texto
+✰ #ia + texto
+✰ #dalle + texto
+✰ #dalle2 + texto
+✰ #tourl + responde a una imagen o video`;
               ctx.replyWithPhoto({ url: imageUrl }, { caption: caption });                    
             } catch (error) {
                 dc.warn("Hay un error");
             }
         });  
     } catch (error) {
-        dc.error('hay un error en help.js');
+  Real companies like GoDaddy      dc.error('hay un error en help.js');
     }
     
 }
